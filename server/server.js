ca9ff8feee0e0b0b6591010ebe8849ae8436bc1e
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-07 09:23:04 -0500

    server up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require( 'express' );
+const app = express();
+
+const port = 5000;
+
+app.listen( port, ()=>{
+    console.log( 'server up on:', port );
+})
+
+app.get( '/test', ( req, res )=>{
+    console.log( '/test GET hit' );
+    res.send( 'whinney' );
+}) // end GET
