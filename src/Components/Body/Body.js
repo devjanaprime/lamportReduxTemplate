c6cb4b6f023b071f9145b6c3abd36e687e908b95
Dev Jana <devjana@Devs-MBP-2.domain>
2020-05-07 11:57:25 -0500

    added body dispatch

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import { Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import 'typeface-roboto';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider }from '@material-ui/core/styles/';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { purple } from '@material-ui/core/colors';
 import { yellow } from '@material-ui/core/colors';
@@ -14,16 +15,38 @@ const myTheme = createMuiTheme({
 }) // end theme
 
 class Body extends Component{
+  state={
+    name:''
+  }
+
   componentDidMount(){
     console.log( this.props );
   }
+
+  handleChange = ( event ) =>{
+    console.log( 'in handleChange:', event.target.value );
+    this.setState({
+      name: event.target.value
+    }) // end setState
+  } // end handleChange
+
+  handleClick = () =>{
+    console.log( 'in handleClick' );
+    this.props.dispatch( 
+      { 
+        type: 'setName',
+        payload: this.state.name
+      } // end object being dispatched
+    ) // end dispatch
+  } // end handle click
+
   render() {
     return (
       <div>
         <h1>Body.js</h1>
         <MuiThemeProvider theme={ myTheme }>
-          <Button color="primary" variant="outlined">Button 1</Button>
-          <Button color="secondary" variant="outlined">Button 2</Button>
+          <TextField placeholder="name" onChange={ this.handleChange }></TextField>
+          <Button color="primary" variant="outlined" onClick={ this.handleClick }>Save Name</Button>
         </MuiThemeProvider>
       </div>
     ); // end return
